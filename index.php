68fba47e38e0341bd506604bea787fe2fe807f4d
danielhaish <danielhaish@gmail.com>
2023-03-22 14:05:13 +0200

    Dev (#1)
    * Create index.php
    
    * Create add_offer.php
    
    * Add

---
+++ index.php
@@ -0,0 +1,133 @@
+<php>
+<div class="topnav">
+
+  <!-- Centered link -->
+  <div class="topnav-centered">
+   <button> Take product photo</button>
+   <button> place order</button>
+   <div class="slidecontainer">
+       <h1>price per kg:</h1>
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
+   <div class="slidecontainer">
+       <h1>max price:</h1>
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
+       <div class="slidecontainer">
+       <h1>min price:</h1>
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
+  <div class="slidecontainer">
+       <h1>exprassion date:</h1>
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
+<textarea>description:</textarea>
+  </div>
+
+  <!-- Left-aligned links (default) -->
+ 
+
+  <!-- Right-aligned links -->
+  <div class="topnav-right">
+    <a href="#search">Search</a>
+    
+  </div>
+
+</div>
+<style>
+    .slidecontainer {
+  width: 100%; /* Width of the outside container */
+}
+
+/* The slider itself */
+.slider {
+  -webkit-appearance: none;  /* Override default CSS styles */
+  appearance: none;
+  width: 100%; /* Full-width */
+  height: 25px; /* Specified height */
+  background: #d3d3d3; /* Grey background */
+  outline: none; /* Remove outline */
+  opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+  -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+  transition: opacity .2s;
+}
+
+/* Mouse-over effects */
+.slider:hover {
+  opacity: 1; /* Fully shown on mouse-over */
+}
+
+/* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none; /* Override default look */
+  appearance: none;
+  width: 25px; /* Set a specific slider handle width */
+  height: 25px; /* Slider handle height */
+  background: #04AA6D; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+}
+
+.slider::-moz-range-thumb {
+  width: 25px; /* Set a specific slider handle width */
+  height: 25px; /* Slider handle height */
+  background: #04AA6D; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+}
+    .topnav {
+  position: relative;
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+/* Centered section inside the top navigation */
+.topnav-centered a {
+  float: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+/* Right-aligned section inside the top navigation */
+.topnav-right {
+  float: right;
+}
+
+/* Responsive navigation menu - display links on top of each other instead of next to each other (for mobile devices) */
+@media screen and (max-width: 600px) {
+  .topnav a, .topnav-right {
+    float: none;
+    display: block;
+  }
+
+  .topnav-centered a {
+    position: relative;
+    top: 0;
+    left: 0;
+    transform: none;
+  }
+}
+</style>
+</php>
